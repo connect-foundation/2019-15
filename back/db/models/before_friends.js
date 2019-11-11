48d78c91e3dc9108b4afdec7a10be549d0732b56
EasyZero <eminem54@naver.com>
2019-11-11 18:36:12 +0900

    fix: before_friends table setting
    친구 신청 테이블 users 테이블에 대해 제약 추가
    친구 신청 테이블 시드 추가

--- back/db/models/before_friends.js
+++ back/db/models/before_friends.js
@@ -1,12 +1,27 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const before_friends = sequelize.define('before_friends', {
-    p_friend_id: DataTypes.INTEGER,
-    s_friend_id: DataTypes.INTEGER,
-    friend_state_id: DataTypes.INTEGER
-  }, {});
+  const before_friends = sequelize.define(
+    'before_friends',
+    {
+      p_friend_id: DataTypes.INTEGER,
+      s_friend_id: DataTypes.INTEGER,
+      friend_state_id: DataTypes.INTEGER,
+    },
+    {},
+  );
   before_friends.associate = function(models) {
-    // associations can be defined here
+    before_friends.belongsTo(models.users, {
+      as: 'p_friend_id',
+      foreignKey: 'id',
+    });
+    before_friends.belongsTo(models.users, {
+      as: 's_friend_id',
+      foreignKey: 'id',
+    });
+    before_friends.belongsTo(models.friend_states, {
+      as: 'friend_state_id',
+      foreignKey: 'id',
+    });
   };
   return before_friends;
 };
