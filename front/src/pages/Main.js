f42c0102e8258c632fb00b44bb8c77138f92af07
hos101010 <hos101010@naver.com>
2019-11-13 20:16:36 +0900

    feat: pop up friends div
    친구 목록을 띄우기위해
    버튼을 누르면 div가 나오는 부분까지만 해놓음

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import RoomSelectSection from '../components/RoomSelectSection/RoomSelectSection';
+import FriendsSection from '../components/FriendsSection/FriendsSection';
 
 const Main = () => (
   <>
     <NavigationBar />
     <RoomSelectSection />
+    <FriendsSection />
   </>
 );
 
