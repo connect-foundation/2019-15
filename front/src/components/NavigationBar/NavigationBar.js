df5a49728559c73fc4cba14371f075dccd3061fe
Einere <kjwsx23@gmail.com>
2019-11-14 19:02:41 +0900

    style(navigation bar): add log out button to navigation bar
    로그아웃을 누를 수 있도록, 네비게이션 바에 로그아웃 버튼을 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/NavigationBar/NavigationBar.js
+++ front/src/components/NavigationBar/NavigationBar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import NavigationBarStyle from './NavigationBar.style';
 import mainlogo from '../../asset/mainlogo.png';
 import Image from '../globalComponents/Image/Image';
+import NavigationBarButton from './NavigationBarButton/NavigationBarButton';
 
 const NavigationBar = () => (
   <NavigationBarStyle>
     <Image src={mainlogo} />
+    <NavigationBarButton />
   </NavigationBarStyle>
 );
 
