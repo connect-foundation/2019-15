27a5e4d1f6501370785f048f7b64f70ddd93c52e
EasyZero <eminem54@naver.com>
2019-11-14 16:21:32 +0900

    style: add room button style
    랜덤게임, 비밀게임에 들어가는 버튼용 스타일 파일 추가

---
+++ front/src/components/RoomSelectSection/PublicRoom/PublicRoomButton.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Button from '../../globalComponents/Button/Button.style';
+
+const PublicRoomButton = styled(Button)`
+  width: 40%;
+  height: 30%;
+  font-size: 3rem;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export default PublicRoomButton;
