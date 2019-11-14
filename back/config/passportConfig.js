9d7b742fb3d6632a5333c017aba327cc4016caf6
changgunyee <minyoung403@naver.com>
2019-11-14 11:40:13 +0900

    typo: remove local from routing envs
    환경에 따라 다른 변수가 유동적으로 주입됨으로 local이 아님

--- back/config/passportConfig.js
+++ back/config/passportConfig.js
@@ -1,20 +1,13 @@
-let SERVER_URI = process.env.DB_LOCAL_HOST;
-if (process.env.NODE_ENV === 'production') {
-  SERVER_URI = process.env.DB_PROD_HOST;
-} else if (process.env.NODE_ENV === 'development') {
-  SERVER_URI = process.env.DB_DEV_HOST;
-}
-
 const passportConfig = {
   facebookConfig: {
     clientID: process.env.PASSPORT_FACEBOOK_CLIENT_ID,
     clientSecret: process.env.PASSPORT_FACEBOOK_CLIENT_SECRET,
-    callbackURL: `${process.env.EXPRESS_APP_LOCAL_URI}/auth/facebook/callback`,
+    callbackURL: `${process.env.EXPRESS_APP_URI}/auth/facebook/callback`,
   },
   googleConfig: {
     clientID: process.env.PASSPORT_GOOGLE_CLIENT_ID,
     clientSecret: process.env.PASSPORT_GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.EXPRESS_APP_LOCAL_URI}/auth/google/callback`,
+    callbackURL: `${process.env.EXPRESS_APP_URI}/auth/google/callback`,
   },
 };
 
