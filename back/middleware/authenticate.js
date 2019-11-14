501a9490b3a5b0968f8fb87c0c1e38c9acc60100
Einere <kjwsx23@gmail.com>
2019-11-14 13:38:51 +0900

    fix(auth): fix authenticate logic
    status code를 2번 보내는 경우가 발생하여, else를 이용해 한번만 보내도록 수정함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- back/middleware/authenticate.js
+++ back/middleware/authenticate.js
@@ -2,14 +2,15 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res) => {
   if (!req.user || !req.user.token) res.sendStatus(401);
-
-  try {
-    jwt.verify(req.user.token, process.env.JWT_SECRET, {
-      issuer: 'catchMyMind',
-      subject: 'userInfo',
-    });
-    res.sendStatus(200);
-  } catch (e) {
-    res.sendStatus(403);
+  else {
+    try {
+      jwt.verify(req.user.token, process.env.JWT_SECRET, {
+        issuer: 'catchMyMind',
+        subject: 'userInfo',
+      });
+      res.sendStatus(200);
+    } catch (e) {
+      res.sendStatus(403);
+    }
   }
 };
