6ad50f74c43fd616b891cf3bd8125f2a4888275c
Einere <kjwsx23@gmail.com>
2019-11-15 16:57:46 +0900

    typo(logout): modify `NavigationBarButton` to `LogOutButton`
    해당 컴포넌트의 역할을 좀 더 명확하게 하기 위해, 이름을 변경함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/NavigationBar/LogOutButton/LogOutButton.js
+++ front/src/components/NavigationBar/LogOutButton/LogOutButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../../globalComponents/Button/Button';
 
-const NavigationBarButton = () => {
+const LogOutButton = () => {
   // logics로 분리 예정
   function logout() {
     window.location.href = `${process.env.REACT_APP_LOCAL_API_URI}/auth/logout`;
@@ -10,4 +10,4 @@ const NavigationBarButton = () => {
   return <Button onClick={logout}>로그아웃</Button>;
 };
 
-export default NavigationBarButton;
+export default LogOutButton;
