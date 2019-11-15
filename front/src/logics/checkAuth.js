5f6fa348eb79282b48166fa93066018985d6e725
Einere <kjwsx23@gmail.com>
2019-11-15 15:09:36 +0900

    fix(auth): use template string instead of hard coding
    유지보수성을 위해, 템플릿 스트링을 사용하도록 수정함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/logics/checkAuth.js
+++ front/src/logics/checkAuth.js
@@ -1,14 +1,14 @@
 function checkAuth() {
-  fetch('http://localhost:3001/auth/authenticate', {
-    // fetch(`${process.env.EXPRESS_APP_URI}/auth/authenticate`, {
+  fetch(`${process.env.REACT_APP_LOCAL_API_URI}/auth/authenticate`, {
     method: 'GET',
     credentials: 'include',
   })
-    .then(response => {
-      if (response.status === 401 || response.status === 403)
-        window.location.href = 'http://localhost:3000/';
+    .then((response) => {
+      if (response.status === 401 || response.status === 403) {
+        window.location.href = `${process.env.REACT_APP_URI}`;
+      }
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 }
 
 export default checkAuth;
