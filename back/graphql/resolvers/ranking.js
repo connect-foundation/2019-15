48b0ba08dbb7db882e72565620ac67104cab8bef
changgunyee <minyoung403@naver.com>
2019-11-17 02:13:55 +0900

    feat: change ranking offset pagination to cursor pagination
    offset pagination의 오류들을 cursor based로 변경하면 잡을 수 있다

--- back/graphql/resolvers/ranking.js
+++ back/graphql/resolvers/ranking.js
@@ -1,16 +1,61 @@
 const Sequelize = require('sequelize');
 
+const { Op } = Sequelize;
+const MAX_SCORE = 999999999;
+const MIN_ID = 0;
+
 module.exports = {
   Query: {
-    ranking: (obj, { order, limit, offset }, { Users }) => {
+    ranking: async (obj, { order, first, after }, { Users }) => {
+      const totalCountArr = await Users.findAll({
+        attributes: [
+          [Sequelize.fn('COUNT', Sequelize.col('id')), 'totalCount'],
+        ],
+      });
+
       let scoreOrdering = ['score', 'DESC'];
       if (order === 'ASC') scoreOrdering = ['score'];
+      const id = after ? parseInt(after.slice(0, 10), 10) : MIN_ID;
+      const score = after ? parseInt(after.slice(10, 20), 10) : MAX_SCORE;
 
-      return Users.findAll({
-        offset,
-        limit,
-        order: [scoreOrdering],
+      const nodes = await Users.findAll({
+        where: {
+          [Op.and]: [
+            {
+              id: {
+                [Op.ne]: id,
+              },
+            },
+            {
+              score: {
+                [Op.lte]: score,
+              },
+            },
+          ],
+        },
+        order: [scoreOrdering, Sequelize.col('id')],
+        limit: first,
       });
+
+      const edgesWithCursor = nodes.map((node) => {
+        return {
+          node,
+          cursor:
+            node.id.toString().padStart(10, '0') +
+            node.score.toString().padStart(10, '0'),
+        };
+      });
+
+      return {
+        totalCount: totalCountArr[0].dataValues.totalCount,
+        edges: edgesWithCursor,
+        pageInfo: {
+          endCursor: edgesWithCursor.length
+            ? edgesWithCursor[edgesWithCursor.length - 1].cursor
+            : null,
+          hasNextPage: edgesWithCursor.length === first,
+        },
+      };
     },
   },
 };
