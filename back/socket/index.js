ea67e7b702df55df3b53138112fef5d1aef07cc1
EasyZero <eminem54@naver.com>
2019-11-16 14:25:41 +0900

    fix: socket message type matching
    소켓 메세지 _n명 으로 통일

--- back/socket/index.js
+++ back/socket/index.js
@@ -9,16 +9,16 @@ function personEnterRoom(socket, messageToClient, capacity) {
 function initSocketIO(io) {
   io.on('connection', (socket) => {
     socket.on('enter_3명', (data) => {
-      personEnterRoom(socket, 'connect_3', 3);
+      personEnterRoom(socket, 'connect_3명', 3);
     });
     socket.on('enter_6명', (data) => {
-      personEnterRoom(socket, 'connect_6', 6);
+      personEnterRoom(socket, 'connect_6명', 6);
     });
     socket.on('enter_12명', (data) => {
-      personEnterRoom(socket, 'connect_12', 12);
+      personEnterRoom(socket, 'connect_12명', 12);
     });
     socket.on('enter_100명', (data) => {
-      personEnterRoom(socket, 'connect_100', 100);
+      personEnterRoom(socket, 'connect_100명', 100);
     });
   });
 }
