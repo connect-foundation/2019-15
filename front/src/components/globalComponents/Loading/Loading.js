4d1344f2ef70cfb3e4cc7c6cd38b50a753e68b76
changgunyee <minyoung403@naver.com>
2019-11-16 18:01:15 +0900

    feat: create loading component
    데이터 받아올 동안 사용할 loading컴포넌트 구현

---
+++ front/src/components/globalComponents/Loading/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+import LoadingStyle from './Loading.style';
+
+const Loading = () => <LoadingStyle icon={faCog} spin size="3x" />;
+
+export default Loading;
---
+++ front/src/components/globalComponents/Loading/Loading.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const LoadingStyle = styled(FontAwesomeIcon)`
+  color: ${(props) => props.theme.coldPurple};
+`;
+
+export default LoadingStyle;
