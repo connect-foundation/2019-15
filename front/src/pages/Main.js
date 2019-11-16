f453fedc3bccece2a876d695e3590834a5651be7
EasyZero <eminem54@naver.com>
2019-11-16 14:03:50 +0900

    feat: create socket context
    메인페이지에서 소켓 사용을 위해 컨텍스트 api로 io객체 초기화 후 컨텍스트에 등록

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import RoomSelectSection from '../components/RoomSelectSection/RoomSelectSection';
+import io from '../logics/socketLogic';
 
-const Main = () => (
-  <>
-    <NavigationBar />
-    <RoomSelectSection />
-  </>
-);
+export const MainSocketContext = React.createContext();
+
+const Main = () => {
+  const initSocket = async () => {
+    await io.connectSocket();
+    await io.initMsgHandler();
+  };
+
+  useEffect(() => {
+    initSocket();
+  }, []);
+
+  return (
+    <MainSocketContext.Provider value={{ io }}>
+      <NavigationBar />
+      <RoomSelectSection />
+    </MainSocketContext.Provider>
+  );
+};
 
 export default Main;
