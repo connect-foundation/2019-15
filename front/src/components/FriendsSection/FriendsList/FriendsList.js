dcc2f1c050f37b7a8b2272cbefe391be9d0269b6
hos101010 <hos101010@naver.com>
2019-11-17 06:47:31 +0900

    feat: implement UI about adding/deleting friends
    친구 리스트에서 친구 설정을 할 수 있도록
    설정 아이콘 누르면 설정모드로 들어감
    설정 모드에서는 친구 추가/삭제 버튼이 생김
    전의 style commit에서 만든 것들을 사용

--- front/src/components/FriendsSection/FriendsList/FriendsList.js
+++ front/src/components/FriendsSection/FriendsList/FriendsList.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { faCog, faUserPlus, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 import FriendsListStyle from './FriendsList.style';
 import FriendComponent from './FriendComponent/FriendComponent';
-import getFriends from '../../../logics/friends'
+import IconStyle from './Icons.style';
+import DoneButtonStyle from './DoneButton.style';
+import InputStyle from './Input.style';
+import getFriends from '../../../logics/friends';
 
-const FriendsList = props => {
+const FriendsList = (props) => {
   const [friends, setFriends] = useState([]);
+  const [configMode, switchMode] = useState(false);
 
   useEffect(() => {
     const fetchItems = async () => {
-      const newItems = await getFriends(1);         //getFriends함수 인자로 현재 user의 id 넘겨줘야함
+      const newItems = await getFriends(4); // getFriends함수 인자로 현재 user의 id 넘겨줘야함
       setFriends(newItems.data.friends);
     };
     fetchItems();
   }, []);
 
+  function changeMode() {
+    switchMode(!configMode);
+  }
+
   return (
     <>
-      <FriendsListStyle isVisible={props.isVisible}>
-        {friends.map((friend,idx)=>{
-        return (<FriendComponent key={idx}>{friend.nickname}</FriendComponent>);
-        })}
+      <FriendsListStyle>
+        {configMode
+          ? (
+          <>
+            <FriendComponent>
+              <DoneButtonStyle onClick={changeMode}>완료</DoneButtonStyle>
+            </FriendComponent>
+            <FriendComponent>
+              <InputStyle type="text" />
+              <IconStyle icon={faUserPlus}/>
+            </FriendComponent>
+          </>
+          ) : (
+          <>
+            <FriendComponent>
+              <span>친구 목록</span>
+              <IconStyle icon={faCog} onClick={changeMode} />
+            </FriendComponent>
+          </>
+        )}
+        {friends.map((friend, idx) => (
+          <FriendComponent key={idx}>
+            {friend.nickname}
+            {configMode ? <IconStyle icon={faMinusCircle}/> : null}
+          </FriendComponent>
+        ))}
       </FriendsListStyle>
     </>
   );
