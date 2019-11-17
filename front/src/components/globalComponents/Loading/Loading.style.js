ad9ff2e603c3f6eae6e22dd79c461c2929387400
changgunyee <minyoung403@naver.com>
2019-11-17 17:04:58 +0900

    fix: change loading component color

--- front/src/components/globalComponents/Loading/Loading.style.js
+++ front/src/components/globalComponents/Loading/Loading.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LoadingStyle = styled(FontAwesomeIcon)`
-  color: ${(props) => props.theme.coldPurple};
+  color: ${(props) => props.theme.eastSide};
 `;
 
 export default LoadingStyle;
