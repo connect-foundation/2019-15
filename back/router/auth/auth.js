a90eff4590b965a8f2acc98d7327fe4306606366
changgunyee <minyoung403@naver.com>
2019-11-18 13:34:07 +0900

    env: encrypt .env files for travis
    travis를 위해 .env 2개의 파일을 압축한 후 업로드. 기존의 .env파일들 제거

--- back/router/auth/auth.js
+++ back/router/auth/auth.js
@@ -3,16 +3,17 @@ const facebookRouter = require('./facebook');
 const googleRouter = require('./google');
 const authenticate = require('../../middleware/authenticate');
 const signJWT = require('../../util/signJWT');
+const { REACT_URI } = require('../../config/uri');
 
 const router = express.Router();
 
 router.use('/facebook', facebookRouter);
 router.use('/google', googleRouter);
 
-router.get('/logout', function(req, res, next) {
+router.get('/logout', function (req, res, next) {
   // expiresIn must be string because string is micro seconds but int is seconds
   res.cookie('jwt', signJWT(req, { expiresIn: '0' }));
-  res.redirect(`${process.env.REACT_APP_URI}`);
+  res.redirect(`${REACT_URI}`);
 });
 
 router.get('/authenticate', authenticate);
