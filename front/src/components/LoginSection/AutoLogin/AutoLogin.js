141e9fe983f476027be1bc3b76395707519a4275
Einere <kjwsx23@gmail.com>
2019-11-18 16:40:07 +0900

    fix(log in): inject `setAutoLogIn()` to `AutoLoginStyle`
    자동 로그인을 클릭할 때 마다, 해당 값을 로컬 스토리지에 저장하도록 `setAutoLogIn()`을 주입함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/LoginSection/AutoLogin/AutoLogin.js
+++ front/src/components/LoginSection/AutoLogin/AutoLogin.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AutoLoginStyle from './AutoLogin.style';
+import setAutoLogIn from '../../../logics/setAutoLogIn';
 
 const AutoLogin = () => (
   <>
     <label>
-      <AutoLoginStyle type="checkbox" />
+        <AutoLoginStyle type="checkbox" onClick={setAutoLogIn}/>
       자동 로그인
     </label>
   </>
