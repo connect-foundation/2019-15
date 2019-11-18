f89781ca8a47f24257fb868f58f6c0e1cd2f8b34
changgunyee <minyoung403@naver.com>
2019-11-18 13:48:40 +0900

    typo: fix ENVIRONMENT to ENV

--- front/src/uri.js
+++ front/src/uri.js
@@ -1,9 +1,9 @@
 let REACT_APP_API_URI = process.env.REACT_APP_LOCAL_API_URI;
 let REACT_APP_URI = process.env.REACT_APP_LOCAL_URI;
-if (process.env.REACT_APP_ENVIRONMENT === 'development') {
+if (process.env.REACT_APP_ENV === 'development') {
   REACT_APP_API_URI = process.env.REACT_APP_DEV_API_URI;
   REACT_APP_URI = process.env.REACT_APP_DEV_URI;
-} else if (process.env.REACT_APP_ENVIRONMENT === 'production') {
+} else if (process.env.REACT_APP_ENV === 'production') {
   REACT_APP_API_URI = process.env.REACT_APP_PROD_API_URI;
   REACT_APP_URI = process.env.REACT_APP_PROD_URI;
 }
