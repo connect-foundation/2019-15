78ef9e993587657855bf365998cb73f95a81d97e
Einere <kjwsx23@gmail.com>
2019-11-18 18:56:32 +0900

    feat(log in): add `autoLoginCheckboxEventHandler()` to `AutoLoginStyle`
    자동 로그인 기능을 설저앟기 위해, `autoLoginCheckboxEventHandler()`를 클릭 이벤트 핸들러로 등록함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/LoginSection/AutoLogin/AutoLogin.js
+++ front/src/components/LoginSection/AutoLogin/AutoLogin.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AutoLoginStyle from './AutoLogin.style';
-import setAutoLogIn from '../../../logics/setAutoLogIn';
+import {autoLoginCheckboxEventHandler, getAutoLogIn} from '../../../logics/autoLogIn';
 
 const AutoLogin = () => (
   <>
     <label>
-        <AutoLoginStyle type="checkbox" onClick={setAutoLogIn}/>
+        <AutoLoginStyle type="checkbox" onClick={autoLoginCheckboxEventHandler} defaultChecked={getAutoLogIn()}/>
       자동 로그인
     </label>
   </>
