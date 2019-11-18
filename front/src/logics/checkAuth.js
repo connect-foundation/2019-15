2aae9dfb1f01f62f4bc94ee497bac9f97d11107f
Einere <kjwsx23@gmail.com>
2019-11-18 21:54:48 +0900

    typo: modify `checkAuth.js`
    for oauth test
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/logics/checkAuth.js
+++ front/src/logics/checkAuth.js
@@ -1,13 +1,15 @@
-import APP_URI from '../util/uri'
+import APP_URI from '../util/uri';
 
 function checkAuth() {
+  console.log('test');
   fetch(`${APP_URI.REACT_APP_API_URI}/auth/authenticate`, {
     method: 'GET',
     credentials: 'include',
   })
     .then((response) => {
       if (response.status === 401 || response.status === 403) {
-        window.location.href = `${APP_URI.REACT_APP_URI}`;
+        // window.location.href = `${APP_URI.REACT_APP_URI}`;
+        console.log('test1');
       }
     })
     .catch((error) => console.log(error));
