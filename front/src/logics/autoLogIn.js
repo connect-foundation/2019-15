f7f6799520aefce2930b3707a1b759f1a1fcfdfc
Einere <kjwsx23@gmail.com>
2019-11-19 14:03:35 +0900

    typo: apply lint
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/logics/autoLogIn.js
+++ front/src/logics/autoLogIn.js
@@ -1,13 +1,13 @@
 function autoLoginCheckboxEventHandler(e) {
-    localStorage.setItem('autoLogIn', e.target.checked.toString());
+  localStorage.setItem('autoLogIn', e.target.checked.toString());
 }
 
 function setAutoLogIn(isAutoLogIn) {
-    localStorage.setItem('autoLogIn', isAutoLogIn.toString());
+  localStorage.setItem('autoLogIn', isAutoLogIn.toString());
 }
 
 function getAutoLogIn() {
-    return localStorage.getItem('autoLogIn') === 'true';
+  return localStorage.getItem('autoLogIn') === 'true';
 }
 
-export {autoLoginCheckboxEventHandler, setAutoLogIn, getAutoLogIn};
+export { autoLoginCheckboxEventHandler, setAutoLogIn, getAutoLogIn };
