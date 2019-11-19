b8a521bd865e23a330ee3f4479a621dc2fcbee74
EasyZero <eminem54@naver.com>
2019-11-20 01:37:08 +0900

    feat: add all components to GamePlay page
    게임플레이 페이지 전체 틀만 잡음

--- front/src/pages/Main/GamePlay.js
+++ front/src/pages/Main/GamePlay.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import GameInfo from '../../components/GameInfo/GameInfo';
+import { FlexRowStyle } from '../../components/globalComponents/Container/Flex.style';
+import MainContext from '../../Main.context';
+import Userlist from '../../components/Userlist/Userlist';
+import Canvas from '../../components/Canvas/Canvas';
+import Chatting from '../../components/Chatting/Chatting';
 
 const GamePlay = () => {
-  return <></>;
+  const { room } = useContext(MainContext);
+  return (
+    <>
+      <NavigationBar visible={room.roomType} />
+      <GameInfo />
+      <FlexRowStyle>
+        <Userlist />
+        <Canvas />
+        <Chatting />
+      </FlexRowStyle>
+    </>
+  );
 };
 
 export default GamePlay;
