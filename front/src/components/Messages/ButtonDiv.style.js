28a1d1f7fbbc37ae30ce6b7270b2fd71b1c54994
hos101010 <hos101010@naver.com>
2019-11-20 11:23:38 +0900

    style: add styles about messages user got

---
+++ front/src/components/Messages/ButtonDiv.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ButtonDiv = styled.div`
+  width: 8.2rem;
+  display: flex;
+  justify-content: space-between;
+  margin-top: -0.3rem;
+`;
+
+export default ButtonDiv;
---
+++ front/src/components/Messages/MessageComponent.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MessageComponentStyle = styled.div`
+  height: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default MessageComponentStyle;
