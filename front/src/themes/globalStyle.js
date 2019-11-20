05305b34d45f9e1e2a8d4069976ca312b63a03f8
EasyZero <eminem54@naver.com>
2019-11-20 19:26:18 +0900

    style: add pencil image and surfCrest color to globalStyle

--- front/src/themes/globalStyle.js
+++ front/src/themes/globalStyle.js
@@ -3,6 +3,7 @@ export default {
   navigationBarColor: '#617B88',
   buttonColor: '#3B434C',
   containerColor: '#DBE0DB',
+  surfCrest: '#B1C2B5',
   ecstasy: '#FD7E14',
   eastSide: '#BD83CE',
   amour: '#F7E8F6',
