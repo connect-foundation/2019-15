24da48f7c484b545548ae9cf22440471487de6a6
EasyZero <eminem54@naver.com>
2019-11-20 14:26:49 +0900

    typo: fix variable name
    유저리스트 map 에서 v => user 로 변경

--- front/src/components/Userlist/Userlist.js
+++ front/src/components/Userlist/Userlist.js
@@ -5,7 +5,7 @@ import MainContext from '../../Main.context';
 const Userlist = () => {
   const { userlist } = useContext(MainContext);
 
-  const UserComponents = userlist.map((v) => <div>{v}</div>);
+  const UserComponents = userlist.map((user) => <div>{user}</div>);
 
   return <UserlistStyle>{UserComponents}</UserlistStyle>;
 };
