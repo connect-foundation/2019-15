f0fb2ee9c79f12132a55caecf3080d11d273e0b5
EasyZero <eminem54@naver.com>
2019-11-21 11:05:33 +0900

    feat: add parseCookie util and add User function
    쿠키 파싱해주는 유틸함수와 유저 객체 반환하는 코드 프론트에 추가

---
+++ front/src/util/cookie.js
@@ -0,0 +1,10 @@
+const parseCookies = (cookie = document.cookie) =>
+  cookie
+    .split(';')
+    .map((v) => v.split('='))
+    .reduce((acc, [k, v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
+
+export default parseCookies;
