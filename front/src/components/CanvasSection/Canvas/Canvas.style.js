93cb2b47d5f8637f8c81f7f06cc167ed29e14b2d
hos101010 <hos101010@naver.com>
2019-11-21 18:16:36 +0900

    style: add styles about word choice in game play page
    단어 선택할 때 필요한 style들 정의

---
+++ front/src/components/CanvasSection/Canvas/Canvas.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const CanvasStyle = styled.div`
+  border: solid 2px yellow;
+`;
+
+export default CanvasStyle;
---
+++ front/src/components/CanvasSection/WordChoice/WordCard.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const WordCard = styled.div`
+  display: table;
+  border: 1px solid lightgray;
+  min-width: 4rem;
+  height: 3rem;
+  background-color: white;
+  border-radius: 1rem;
+`;
+
+export const P = styled.p`
+  display: table-cell;
+  text-align: center;
+  vertical-align: middle;
+`;
