2b331c88205fbab0d94151aa63f0ff815b7c7215
Einere <kjwsx23@gmail.com>
2019-11-21 18:53:09 +0900

    style: add style to `Router`
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/Router.js
+++ front/src/Router.js
@@ -1,36 +1,39 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, {useState} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import io from './logics/socketLogic';
 import MainContext from './Main.context';
 
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Main from './pages/Main/Main';
-import MyPage from './pages/MyPage';
+import MyPage from './pages/MyPage/MyPage';
 
 import Room from './logics/room';
+import RouterStyle from './Router.style';
 
 const Router = () => {
   const [userlist, setUserlist] = useState([]);
   const [room, setRoom] = useState(Room());
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/mypage">
-          <MyPage />
-        </Route>
-        <MainContext.Provider
-          value={{ io, userlist, setUserlist, room, setRoom }}
-        >
-          <Route path="/main">
-            <Main />
+    <RouterStyle id={'Router'}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
           </Route>
-        </MainContext.Provider>
-      </Switch>
-    </BrowserRouter>
+          <Route path="/mypage">
+            <MyPage />
+          </Route>
+          <MainContext.Provider
+            value={{ io, userlist, setUserlist, room, setRoom }}
+          >
+            <Route path="/main">
+              <Main />
+            </Route>
+          </MainContext.Provider>
+        </Switch>
+      </BrowserRouter>
+    </RouterStyle>
   );
 };
 
