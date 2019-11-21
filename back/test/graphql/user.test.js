55eee0f62d7f95465c7d3508c006e7811143d674
changgunyee <minyoung403@naver.com>
2019-11-21 18:11:36 +0900

    test: test checkNicknameAvailable query

--- back/test/graphql/user.test.js
+++ back/test/graphql/user.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const uuid = require('uuid/v1');
 
 const graphqlPath = require('../../config/graphqlPath');
 const { app } = require('../../app');
@@ -30,6 +31,40 @@ describe('graphql user resolvers tests', () => {
       },
     ];
     expect(res.body.data.users).toStrictEqual(expect.arrayContaining(usersExpected));
+    done();
+  });
+});
+
+describe('유저 닉네임 중복검사 및 변경 테스트', () => {
+  it('사용 중인 닉네임 중복검사 테스트', async (done) => {
+    const nicknameDuplicated = '강아지';
+    const res = await request
+      .agent(app)
+      .set('Cookie', [`jwt=${token}`])
+      .post(graphqlPath)
+      .send({
+        query: `{
+          checkNicknameAvailable(nickname:"${nicknameDuplicated}")
+        }`,
+      })
+      .expect(200);
+    expect(res.body.data.checkNicknameAvailable).toBeFalsy();
+    done();
+  });
+
+  it('비사용중인 닉네임 중복검사 테스트', async (done) => {
+    const nicknameDuplicated = '고릴라';
+    const res = await request
+      .agent(app)
+      .set('Cookie', [`jwt=${token}`])
+      .post(graphqlPath)
+      .send({
+        query: `query {
+          checkNicknameAvailable(nickname:"${nicknameDuplicated}")
+        }`,
+      })
+      .expect(200);
+    expect(res.body.data.checkNicknameAvailable).toBeFalsy();
     done();
   });
 });
