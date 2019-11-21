20407f7ba9a6164e173f2cd9197745dc8188adb0
Einere <kjwsx23@gmail.com>
2019-11-21 18:55:05 +0900

    style: add style to `RoomContainer`
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/RoomSelectSection/RoomSelectSection.js
+++ front/src/components/RoomSelectSection/RoomSelectSection.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import BackGroundStyle from './BackGroundStyle.style';
 import PublicRoom from './PublicRoom/PublicRoom';
 import PrivateRoom from './PrivateRoom/PrivateRoom';
 
 const RoomSelectSection = () => (
-  <BackGroundStyle>
+  <>
     <PublicRoom />
     <PrivateRoom />
-  </BackGroundStyle>
+  </>
 );
 
 export default RoomSelectSection;
