a51a6ebbbec824e28d182b159698bb7b39b44e0c
Einere <kjwsx23@gmail.com>
2019-11-24 18:29:32 +0900

    feat(character): implement `ChangeCharacter` component
    마이페이지에서, 캐릭터 변경 영역을 위해 해당 컴포넌트를 임시로 구현함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ChangeCharacterStyle from './ChangeCharacter.style';
+
+const ChangeCharacter = () => {
+  return <ChangeCharacterStyle id="ChangeCharacter" />;
+};
+
+export default ChangeCharacter;
---
+++ front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ChangeCharacterStyle = styled.section`
+  background-color: dimgray;
+  width: 100%;
+  height: 100px;
+`;
+
+export default ChangeCharacterStyle;
