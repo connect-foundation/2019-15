b93804c15875c455c82f1e9ac5be86edae9edf31
hos101010 <hos101010@naver.com>
2019-11-24 10:06:45 +0900

    feat: show three random words in client side
    back에서 만든 쿼리를 이용해
    mock이였던 단어들 서버에서 가져옴

--- front/src/components/CanvasSection/WordChoice/WordChoice.js
+++ front/src/components/CanvasSection/WordChoice/WordChoice.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import Background from './Background.style';
 import WordSet from './WordSet.style';
 import { WordCard, P } from './WordCard.style';
-
-// import ModalSection from './ModalSection';
+import getRandomWords from '../../../queries/word';
 
 const WordChoice = () => {
+  const { data, loading, error } = useQuery(getRandomWords);
+  if (loading) {
+    return 'loading';
+  }
+  if (error) {
+    return 'error';
+  }
+
   return (
     <Background>
       <WordSet>
-        <WordCard>
-          <P>word1</P>
-        </WordCard>
-        <WordCard>
-          <P>word2</P>
-        </WordCard>
-        <WordCard>
-          <P>word3</P>
-        </WordCard>
+        {data.getRandomWords.map((word) => (
+          <WordCard key={word.id}>
+            <P>{word.word}</P>
+          </WordCard>
+        ))}
       </WordSet>
     </Background>
   );
