121ea00b0c575b705ff94655ad323df6307720bc
changgunyee <minyoung403@naver.com>
2019-11-24 21:12:43 +0900

    feat: create ToolType component with tool classes and ToolManager class
    사용할 도구와 도구를 관리하는 ToolManage를 class로 구현.
    도구를 선택할 수 있는 ToolType 컴포넌트 구현

---
+++ front/src/components/DrawingPlayGround/Tools/ToolType/ToolType.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ToolManager from './ToolManager';
+import SButton from '../../../globalComponents/SButton/SButton';
+import Tool from './Tool';
+import ToolTypeStyle from './ToolType.style';
+
+const ToolType = ({ tool, changeTool }) => {
+  return (
+    <ToolTypeStyle>
+      {ToolManager.TOOL_LIST().map((toolName) => (
+        <SButton
+          active={toolName === tool.getName()}
+          key={toolName}
+          onClick={() => changeTool(toolName)}
+        >
+          {toolName}
+        </SButton>
+      ))}
+    </ToolTypeStyle>
+  );
+};
+
+ToolType.propTypes = {
+  tool: PropTypes.instanceOf(Tool).isRequired,
+  changeTool: PropTypes.func.isRequired,
+};
+
+export default ToolType;
