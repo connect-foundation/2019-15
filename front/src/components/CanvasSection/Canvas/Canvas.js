9247d8059aba7876d31c1070cc6f630d76e7f96d
hos101010 <hos101010@naver.com>
2019-11-24 09:45:50 +0900

    refactor: put Canvas.js into CanvasSection.js
    CanvasSection에 WordChoice가 같이 들어가기 위해
    Canvas위치 옮김

--- front/src/components/CanvasSection/Canvas/Canvas.js
+++ front/src/components/CanvasSection/Canvas/Canvas.js
@@ -1,20 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import CanvasStyle from './Canvas.style';
-import GamePlayContext from '../../pages/Main/GamePlay.context';
-import GlobalContext from '../../global.context';
 
 const Canvas = () => {
-  const { painter } = useContext(GamePlayContext);
-  const { io } = useContext(GlobalContext);
-
-  useEffect(() => {
-    if (io.socket.id === painter) {
-      return console.log('그릴 차례입니다');
-    }
-
-    return console.log('문제를 맞춰보세요');
-  }, [io.socket.id, painter]);
-
   return (
     <CanvasStyle>
       <div>그림판</div>
--- front/src/components/CanvasSection/CanvasSection.js
+++ front/src/components/CanvasSection/CanvasSection.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Canvas from './Canvas/Canvas';
 import WordChoice from './WordChoice/WordChoice';
 import CanvasSectionStyle from './CanvasSection.style';
+import GamePlayContext from '../../pages/Main/GamePlay.context';
+import GlobalContext from '../../global.context';
 
 const CanvasSection = () => {
+
+  const { painter } = useContext(GamePlayContext);
+  const { io } = useContext(GlobalContext);
+
+  useEffect(() => {
+    if (io.socket.id === painter) {
+      return console.log('그릴 차례입니다');
+    }
+
+    return console.log('문제를 맞춰보세요');
+  }, [io.socket.id, painter]);
+
+
   return (
     <CanvasSectionStyle>
       <WordChoice />
