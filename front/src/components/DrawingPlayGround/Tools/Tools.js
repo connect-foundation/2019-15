5b048359dc4121bd2881d3b9c22f497dea003747
changgunyee <minyoung403@naver.com>
2019-11-24 21:19:41 +0900

    feat: create Tools component
    컬러와 도구를 변경할 수 있는 도구상자 역할의 컴포넌트 구현

---
+++ front/src/components/DrawingPlayGround/Tools/Tools.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ToolsStyle } from './Tools.style';
+import ColorPicker from './ColorPicker/ColorPicker';
+import RGB_LIST from '../../../logics/Tools';
+import ToolType from './ToolType/ToolType';
+import Tool from './ToolType/Tool';
+
+const Tools = ({ drawingOptions, setDrawingOptions }) => {
+  const { tool, strokeColor } = drawingOptions;
+  const changeTool = (toolName) => {
+    setDrawingOptions({ type: 'tool', value: toolName });
+  };
+
+  const changeColor = (rgb) => {
+    setDrawingOptions({ type: 'strokeColor', value: rgb });
+  };
+
+  return (
+    <ToolsStyle>
+      <div>
+        <ToolType tool={tool} changeTool={changeTool} />
+      </div>
+      <div>
+        <ColorPicker
+          rgbList={RGB_LIST}
+          color={strokeColor}
+          changeColor={changeColor}
+        />
+      </div>
+    </ToolsStyle>
+  );
+};
+
+Tools.propTypes = {
+  drawingOptions: PropTypes.shape({
+    tool: PropTypes.instanceOf(Tool).isRequired,
+    strokeColor: PropTypes.string,
+  }),
+  setDrawingOptions: PropTypes.func.isRequired,
+};
+
+Tools.defaultProps = {
+  drawingOptions: PropTypes.shape({
+    strokeColor: '#000000',
+  }),
+};
+
+export default Tools;
