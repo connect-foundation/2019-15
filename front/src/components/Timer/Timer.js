14ce2a0dc0d9f24499235203e64a4ddde9d761f8
Einere <kjwsx23@gmail.com>
2019-11-25 19:04:37 +0900

    fix(timer): add trigger to Timer
    특정 이벤트 발생 시 타이머가 작동하도록, 트리거 함수를 구현함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/Timer/Timer.js
+++ front/src/components/Timer/Timer.js
@@ -3,6 +3,7 @@ import TimerStyle from './Timer.style';
 
 const Timer = () => {
   const [time, setTime] = useState(30);
+  const [flag, setFlag] = useState(false);
   const countDown = () => {
     setTime(time - 1);
   };
@@ -11,14 +12,22 @@ const Timer = () => {
     if (time > 0) setTimeout(countDown, 1000);
   }
 
-  useEffect(() => {
+  function triggerTimer() {
+    setFlag(true);
     runTimer();
+  }
+
+  useEffect(() => {
+    if (flag) runTimer();
   });
 
   return (
-    <TimerStyle>
-      <span>{time}</span>
-    </TimerStyle>
+    <>
+      <TimerStyle>
+        <span>{time}</span>
+      </TimerStyle>
+      <button onClick={triggerTimer}>trigger</button>
+    </>
   );
 };
 
