68630b0a8706cf785a24e432678c0ecec321b698
EasyZero <eminem54@naver.com>
2019-11-27 21:45:13 +0900

    feat: add socket handler for secret game
    비밀게임 방만들기와 게임시작을 위한 socket handler 생성

--- front/src/logics/socketLogic/index.js
+++ front/src/logics/socketLogic/index.js
@@ -5,9 +5,11 @@ import roomInfo from '../room/roomInfo';
 
 const io = {
   socket: null,
+
   async connectSocket() {
     this.socket = await socketIo.connect(`${APP_URI.REACT_APP_API_URI}`);
   },
+
   async getSocket() {
     if (this.socket !== null) {
       return this.socket;
@@ -15,6 +17,7 @@ const io = {
     await this.connectSocket();
     return this.socket;
   },
+
   async initConnectMsgHandler({ setRoom }) {
     roomInfo.roomList.forEach((roomName) => {
       this.socket.on(`connect_${roomName}`, ({ roomType, roomId }) => {
@@ -22,21 +25,35 @@ const io = {
       });
     });
   },
+
   async initUserListMsgHandler({ setUserList }) {
-    this.socket.on('userlist', ({ userList }) => {
+    this.socket.on('userList', ({ userList }) => {
       const parsedList = JSON.parse(userList);
       setUserList(parsedList);
     });
   },
+
   async initGameStartMsgHandler({ setPainter }) {
     this.socket.on('gamestart', ({ painter }) => {
       setPainter(painter);
     });
   },
 
+  async initStartSecretGameHandler({ setPainter, setIsGamePlaying }) {
+    this.socket.on('startSecretGame', ({ painter }) => {
+      setPainter(painter);
+      setIsGamePlaying(true);
+    });
+  },
+
   async requestMakeSecretRoom({ nickname, roomId }) {
     this.socket.emit('make_secret', { nickname, roomId });
   },
+
+  async startSecretGame({ roomId, roomType }) {
+    this.socket.emit('startSecretGame', { roomId, roomType });
+  },
+
   async exitGameRoom({ nickname, roomType, roomId }) {
     this.socket.emit('exit_room', { nickname, roomType, roomId });
   },
