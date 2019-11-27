6a5313bd0be94d13580ab76ae627e42fe9472b0e
EasyZero <eminem54@naver.com>
2019-11-27 22:08:52 +0900

    typo: spelling mistake

--- front/src/logics/socketLogic/index.js
+++ front/src/logics/socketLogic/index.js
@@ -20,7 +20,7 @@ const io = {
 
   async initConnectMsgHandler({ setRoom }) {
     roomInfo.roomList.forEach((roomName) => {
-      this.socket.on(`connect_${roomName}`, ({ roomType, roomId }) => {
+      this.socket.on(`connect${roomName}`, ({ roomType, roomId }) => {
         setRoom(new Room(roomId, roomType));
       });
     });
