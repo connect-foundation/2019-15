fe75dd3b65d4a90fab92c61ff0425d29aea1a301
changgunyee <minyoung403@naver.com>
2019-11-27 17:01:45 +0900

    typo: fix using RankingAllStyle in RankingFriends

--- front/src/components/Ranking/RankingFriends/RankingFriends.js
+++ front/src/components/Ranking/RankingFriends/RankingFriends.js
@@ -4,9 +4,8 @@ import { getRankingFriends } from '../../../queries/ranking';
 import Loading from '../../globalComponents/Loading/Loading';
 import Alert from '../../globalComponents/Alert/Alert';
 import UserRankingList from '../UserRankingList/UserRankingList';
-import Button from '../../globalComponents/Button/Button';
-import RankingAllStyle from '../RankingAll/RankingAll.style';
 import InfinityScroll from '../../globalComponents/InfinityScroll/InfinityScroll';
+import RankingFriendsStyle from './RankingFriends.style';
 
 const RankingFriends = () => {
   const { data, loading, error, fetchMore } = useQuery(getRankingFriends);
@@ -45,11 +44,11 @@ const RankingFriends = () => {
     });
   const users = edges.map((edge) => edge.node);
   return (
-    <RankingAllStyle>
+    <RankingFriendsStyle>
       <InfinityScroll loadMore={loadMore} hasMore={hasNextPage}>
         <UserRankingList users={users} />
       </InfinityScroll>
-    </RankingAllStyle>
+    </RankingFriendsStyle>
   );
 };
 
