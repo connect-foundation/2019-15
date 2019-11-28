c3eccd04a11e5400068fa6b8d604bc58e5b2a93a
EasyZero <eminem54@naver.com>
2019-11-28 22:30:31 +0900

    fix: exit game logic to handling errer
    유저가 나가기를 했을 때 방이 없다면 서버가 죽지 않도록 확실한 종료처리

--- back/socket/index.js
+++ back/socket/index.js
@@ -67,6 +67,7 @@ function initSocketIO(io) {
 
       // 방이 없는 경우
       if (roomIdx < 0) return;
+
       roomInfo = { roomId, roomType };
       const userList = nRooms[roomIdx].people.map((v) => v.id);
 
@@ -86,12 +87,23 @@ function initSocketIO(io) {
       }
     });
 
+    socket.on('gameImage', ({ roomId, image }) => {
+      socket.to(roomId).emit('gameImage', { image });
+    });
+
     socket.on('exitRoom', ({ nickname, roomType, roomId }) => {
-      if (!roomType || !roomId) return;
-      const userList = RoomManager.room[roomType][roomId].players;
+      const rooms = RoomManager.room;
+      if (!roomType) return;
+
+      const room = rooms[roomType];
+      if (!roomId || !(roomId in room)) return;
+
+      const userList = room[roomId].players;
+
       const exitUserIdx = userList.findIndex((user) => user.socket.id === socketId);
       userList.splice(exitUserIdx, 1);
 
+      // 리뷰: 유저리스트를 다보내지 말고 제외된 유저 아이디만 보내자
       sendUserListToRoom(userList, roomId, io);
     });
 
