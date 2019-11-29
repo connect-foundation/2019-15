33545a4e091c00efa5df1a0081aee92f7e1bbe2f
EasyZero <eminem54@naver.com>
2019-11-29 11:56:34 +0900

    현재 소켓이 게임소켓과 접속확인용소켓 네임스페이스로 나뉘어 있는 상태입니다
    소켓관리가 어려운데 소켓을 상태로 관리하는게 나을까요?
    아니면 객체로 따로 관리하는 방법이 나을까요?
    
    객체로 관리를 하다보니 함수가 점점 늘어나는데 분리할 수 있는 좋은 방법이 있을까요?

---
+++ front/socketLogic/index.js
@@ -0,0 +1,92 @@
+import socketIo from 'socket.io-client';
+import Room from '../room';
+import APP_URI from '../../util/uri';
+import roomInfo from '../../constant/room/roomInfo';
+
+const io = {
+  socket: null,
+
+  async connectSocket() {
+    this.socket = await socketIo.connect(`${APP_URI.REACT_APP_API_URI}`);
+  },
+
+  async getSocket() {
+    if (this.socket !== null) {
+      return this.socket;
+    }
+    await this.connectSocket();
+    return this.socket;
+  },
+
+  async initConnectMsgHandler({ setRoom }) {
+    roomInfo.roomList.forEach((roomName) => {
+      this.socket.on(`connect${roomName}`, ({ roomType, roomId }) => {
+        setRoom(new Room(roomId, roomType));
+      });
+    });
+  },
+
+  async initUserListMsgHandler({ setUserList }) {
+    this.socket.on('userList', ({ userList }) => {
+      const parsedList = JSON.parse(userList);
+      setUserList(parsedList);
+    });
+  },
+
+  async initGameStartMsgHandler({ setPainter }) {
+    this.socket.on('gamestart', ({ painter }) => {
+      setPainter(painter);
+    });
+  },
+
+  async initStartSecretGameHandler({ setPainter, setIsGamePlaying }) {
+    this.socket.on('startSecretGame', ({ painter }) => {
+      setPainter(painter);
+      setIsGamePlaying(true);
+    });
+  },
+
+  async requestMakeSecretRoom({ nickname, roomId }) {
+    this.socket.emit('makeSecret', { nickname, roomId });
+  },
+
+  async startSecretGame({ roomId, roomType }) {
+    this.socket.emit('startSecretGame', { roomId, roomType });
+  },
+
+  async exitGameRoom({ nickname, roomType, roomId }) {
+    this.socket.emit('exitRoom', { nickname, roomType, roomId });
+  },
+  async sendMessage({ socketId, roomType, roomId, inputValue }) {
+    this.socket.emit('sendMessage', { socketId, roomType, roomId, inputValue });
+  },
+  async initChattingHandler({ messages, pushMessage }) {
+    this.socket.on('getMessage', ({ content, privileged }) => {
+      const splitRes = content.split(' : ');
+      if (splitRes.length === 2 && splitRes[1] === '') return;
+      messages.push({ content, privileged });
+      pushMessage(messages.slice());
+    });
+  },
+  async selectWord({ answer, roomType, roomId }) {
+    this.socket.emit('selectWord', { answer, roomType, roomId });
+  },
+  async setStartQuestionHandler(setQuestionWord, callback) {
+    this.socket.on('startQuestion', ({ wordLength, openLetter, openIndex }) => {
+      setQuestionWord({ wordLength, openLetter, openIndex });
+      callback();
+    });
+  },
+
+  async initImageSendHandler({ setCanvasImage }) {
+    this.socket.on('gameImage', ({ image }) => {
+      setCanvasImage({ image });
+    });
+  },
+
+  async sendImage({ roomId, image }) {
+    await this.socket.emit('gameImage', { roomId, image });
+  },
+};
+
+export default io;
