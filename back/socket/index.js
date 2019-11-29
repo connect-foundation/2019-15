fc717b94f94aa093aa263ef02f23f3d3be5747ea
EasyZero <eminem54@naver.com>
2019-11-29 12:02:38 +0900

    객체에 방이 없을 때를 대비한 return 처리입니다
    별개로 try-catch 처리를 해주는게 좋을까요?

--- back/socket/index.js
+++ back/socket/index.js
@@ -77,6 +77,21 @@ function initSocketIO(io) {
       socket.emit('userList', { userList: JSON.stringify(userList) });
     });
 
+    socket.on('exitRoom', ({ nickname, roomType, roomId }) => {
+      const rooms = RoomManager.room;
+      if (!roomType) return;
+
+      const room = rooms[roomType];
+      if (!roomId || !(roomId in room)) return;
+
+      const userList = room[roomId].players;
+
+      const exitUserIdx = userList.findIndex((user) => user.socket.id === socketId);
+      userList.splice(exitUserIdx, 1);
+
+      // 리뷰: 유저리스트를 다보내지 말고 제외된 유저 아이디만 보내자
+      sendUserListToRoom(userList, roomId, io);
+    });
   });
 }
 
