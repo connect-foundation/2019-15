6d9bb4c90cd81429ca540a65204c0bd4e6f53654
EasyZero <eminem54@naver.com>
2019-11-29 12:08:06 +0900

    그 외 소켓 에러 처리가 전혀 안되고 있는데 현재 코드에 문제가 함수가 너무 큰것같습니다
    처음에는 서버니까 메세지는 한번에 등록시켜주면 되겠지
    생각했는데
    생각보다 메세지가 많아지면서 함수분리만으로 힘들것 같습니다
    
    분리하는 좋은 방법이 있을까요?

--- back/socket/index.js
+++ back/socket/index.js
@@ -77,6 +77,23 @@ function initSocketIO(io) {
       socket.emit('userList', { userList: JSON.stringify(userList) });
     });
 
+    socket.on('makeSecret', ({ nickname, roomId }) => {
+      personEnterSecretRoom(nickname, socket, roomId, io);
+      userName = nickname;
+      roomInfo = { roomId, roomType: '비밀방' };
+    });
+
+    socket.on('startSecretGame', ({ roomId, roomType }) => {
+      const room = RoomManager.room[roomType][roomId];
+      if (room.players.length >= 2) {
+        io.to(roomId).emit('startSecretGame', { painter: room.players[0].socket.id });
+      }
+    });
+
+    socket.on('gameImage', ({ roomId, image }) => {
+      socket.to(roomId).emit('gameImage', { image });
+    });
+
     socket.on('exitRoom', ({ nickname, roomType, roomId }) => {
       const rooms = RoomManager.room;
       if (!roomType) return;
@@ -92,6 +109,7 @@ function initSocketIO(io) {
       // 리뷰: 유저리스트를 다보내지 말고 제외된 유저 아이디만 보내자
       sendUserListToRoom(userList, roomId, io);
     });
+
     socket.on('disconnect', () => {
       if (roomInfo) {
         const userList = RoomManager.room[roomInfo.roomType][roomInfo.roomId].players;
@@ -101,7 +119,57 @@ function initSocketIO(io) {
           sendUserListToRoom(userList, roomInfo.roomId, io);
         }
       }
+    });
+
+    socket.on('sendMessage', ({ socketId, roomType, roomId, inputValue }) => {
+      let answer;
+      try {
+        answer = RoomManager.room[roomType][roomId].word;
+      } catch {
+        answer = null;
+      }
+      RoomManager.room[roomType][roomId].players.findIndex((user) => {
+        if (user.socket.id === socketId) {
+          if (inputValue === answer && !user.privileged) {
+            user.privileged = true;
+            io.in(roomId).emit('getMessage', {
+              content: `${user.nickname}님이 정답을 맞췄습니다! Hooray`,
+              privileged: 'notice',
+            });
+          } else {
+            io.in(roomId).emit('getMessage', {
+              content: `${user.nickname} : ${inputValue}`,
+              privileged: user.privileged,
+            });
+          }
+        }
+      });
+    });
+
+    socket.on('selectWord', ({ answer, roomType, roomId }) => {
+      const room = RoomManager.room[roomType][roomId];
+      room.word = answer;
+      // 서버 타이머 트리거
+      room.timer.start();
+      // 클라들에게 뿌려주기
+      const openIndex = getRandomInt(0, answer.length);
+      io.in(roomId).emit('startQuestion', {
+        wordLength: answer.length,
+        openLetter: answer[openIndex],
+        openIndex,
+      });
+    });
+
+    // 출제자가 캔버스에 그림을 그리는 경우.
+    socket.on('drawing', ({ roomId }) => {
+      // 출제자를 제외한 참가자들에게 캔버스 정보를 전송
+      io.to(roomId).emit('drawing');
+    });
   });
+
+  // onlineIo
+  this.io = io;
+  this.onlineIo = io.of('/online').on('connection', setOnlineSockets.bind(this));
 }
 
 module.exports = initSocketIO;
