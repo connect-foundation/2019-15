6a125f13b7ae3d2d20e6de1bfd86c1fef64dcb53
EasyZero <eminem54@naver.com>
2019-12-04 16:43:02 +0900

    refactor: move lower components of GamePlay

--- front/src/components/GamePlay/GamePlay.js
+++ front/src/components/GamePlay/GamePlay.js
@@ -3,9 +3,9 @@ import { Redirect } from 'react-router-dom';
 import NavigationBar from 'components/NavigationBar/NavigationBar';
 import { FlexRowStyle } from 'components/globalComponents/Container/Flex.style';
 import GlobalContext from 'global.context';
-import UserList from 'components/Userlist/Userlist';
+import UserList from 'components/GamePlay/Userlist/Userlist';
 import CanvasSection from 'components/GamePlay/CanvasSection/CanvasSection';
-import Chatting from 'components/Chatting/Chatting';
+import Chatting from 'components/GamePlay/Chatting/Chatting';
 import GamePlayContext from 'components/GamePlay/GamePlay.context';
 import GameLoading from 'components/GamePlay/GameLoading/GameLoading';
 
