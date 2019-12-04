b2559d6f3b5483cb469a2730e8dc8ad4ceccc334
hos101010 <hos101010@naver.com>
2019-12-04 13:58:58 +0900

    style: fix background to suitable components

--- front/src/components/CanvasSection/DrawingPlayGround/NonPainterBoard/NonPainterBoard.style.js
+++ front/src/components/CanvasSection/DrawingPlayGround/NonPainterBoard/NonPainterBoard.style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const NonPainterBoardStyle = styled.div`
+  background-color: white;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 10px, rgba(0, 0, 0, 0.16) 0px 2px 5px;
 `;
 
