224adb39149ba0a0399d72ae81e8e342dc834a1d
Einere <kjwsx23@gmail.com>
2019-12-04 18:18:17 +0900

    feat(question result): make `QuestionResult` component
    문제가 종료되었을 때, 해당 문제의 정답과 랭킹을 보여주기 위한 컴포넌트를 구현함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

---
+++ front/src/components/GamePlay/CanvasSection/QuestionResult/QuestionResult.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import makeModal from '../../../globalComponents/Modal/Modal';
+
+QuestionResult.propTypes = {
+  answer: PropTypes.string.isRequired,
+  scores: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  ).isRequired,
+};
+
+function QuestionResult({ answer, scores }) {
+  const Header = () => <h1>{answer}</h1>;
+  const Body = () =>
+    scores.map((score) => (
+      <div>
+        {score[0]} : {score[1]}
+      </div>
+    ));
+
+  const Modal = makeModal(Header, Body);
+
+  return <Modal />;
+}
+
+export default QuestionResult;
