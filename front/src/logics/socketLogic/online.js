564371cca829ad11940ae0a51336b24af9615b1b
changgunyee <minyoung403@naver.com>
2019-12-04 19:43:47 +0900

    refactor: make useAlarm hook and Notice component

--- front/src/logics/socketLogic/online.js
+++ front/src/logics/socketLogic/online.js
@@ -5,21 +5,20 @@ export const connectSocket = () => {
   return socketIo.connect(`${APP_URI.REACT_APP_API_URI}/online`);
 };
 
-export const onRequestFriend = (
-  socket,
-  { alarmListDispatch, setNoticeType },
-) => {
+export const onRequestFriend = (socket, { alarmListDispatch }) => {
   socket.on('requestFriend', (user) => {
     alarmListDispatch({
       type: 'push',
       value: `${user.nickname}님이 친구 신청하였습니다.`,
     });
-    setNoticeType('alarm');
     setTimeout(() => {
       alarmListDispatch({ type: 'pop' });
-      setNoticeType(null);
-    }, 2000);
+    }, 10000);
   });
+};
+
+export const offRequestFriend = (socket) => {
+  socket.off('requestFriend');
 };
 
 export const emitRequestFriend = (socket, { sender, receiver }) => {
