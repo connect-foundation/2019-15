99c5a714d520b79d00fff78d989ec984ee4d6e3a
hos101010 <hos101010@naver.com>
2019-12-04 10:51:18 +0900

    style: make canvas color white

--- front/src/components/CanvasSection/DrawingPlayGround/DrawingPlayGround.style.js
+++ front/src/components/CanvasSection/DrawingPlayGround/DrawingPlayGround.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const childrenMargin = '0.5rem';
 
 const DrawingPlayGroundStyle = styled.div`
+  background-color: white;
   > * {
     margin: ${childrenMargin};
   }
