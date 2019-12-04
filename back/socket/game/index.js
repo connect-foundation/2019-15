9c3b377308a2f7599ebd53794ee073e69cec29e9
Einere <kjwsx23@gmail.com>
2019-12-04 15:07:54 +0900

    fix(socket): modify arguments
    아규먼트를 roomInfo 하나만 전달하도록 수정함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- back/socket/game/index.js
+++ back/socket/game/index.js
@@ -39,7 +39,7 @@ function setGameSocket(socket) {
   socket.on('exitRoom', exitRoom.bind(this, gameSocket));
   socket.on('disconnect', () => {
     if (!roomInfo) return;
-    if (!RoomManager.isExistRoom(roomInfo.roomType, roomInfo.roomId)) return;
+    if (!RoomManager.isExistRoom(roomInfo)) return;
 
     const { roomType, roomId } = roomInfo;
     const room = RoomManager.room[roomType][roomId];
