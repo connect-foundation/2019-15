679ffeb6e39435d076070e95276ec3dbb4645a47
hos101010 <hos101010@naver.com>
2019-12-04 18:26:20 +0900

    style: put wordpreview center

--- front/src/components/CanvasSection/WordPreview/WordPreview.style.js
+++ front/src/components/CanvasSection/WordPreview/WordPreview.style.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-const WordPreviewStyle = styled.section``;
+const WordPreviewStyle = styled.section`
+    width: 20rem;
+    text-align: center;
+    font-size: 30px;
+`;
 
 export default WordPreviewStyle;
