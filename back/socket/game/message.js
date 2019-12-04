336f57be8bd28b6c074ff30710909d6e5315eafa
Einere <kjwsx23@gmail.com>
2019-12-04 11:42:23 +0900

    fix(room): add count answererCount
    정답을 맞춘 경우, 정답차의 수를 증가시키는 코드를 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- back/socket/game/message.js
+++ back/socket/game/message.js
@@ -8,7 +8,7 @@ function sendMessage(gameSocket, { roomType, roomId, inputValue }) {
 
   const room = this.RoomManager.room[roomType][roomId];
   const idx = room.getUserIndexBySocketId(gameSocket);
-  if (idx <= 0) return;
+  if (idx < 0) return;
 
   const player = room.players[idx];
 
@@ -21,7 +21,11 @@ function sendMessage(gameSocket, { roomType, roomId, inputValue }) {
     player.privileged = true;
     returnMessage.content = `${player.nickname}님이 정답을 맞췄습니다! Hooray`;
     returnMessage.privileged = 'notice';
+    room.answererCount += 1;
+
+    // 모든 플레이어가 답을 맞춘 경우
   }
+
   this.gameIo.in(roomId).emit('getMessage', returnMessage);
 }
 
