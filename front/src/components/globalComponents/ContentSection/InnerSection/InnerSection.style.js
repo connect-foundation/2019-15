138c262f241e4c65ae343ac43ae3999cf67e5102
hos101010 <hos101010@naver.com>
2019-12-05 19:00:07 +0900

    style: make currentInfo aligned

--- front/src/components/globalComponents/ContentSection/InnerSection/InnerSection.style.js
+++ front/src/components/globalComponents/ContentSection/InnerSection/InnerSection.style.js
@@ -7,6 +7,10 @@ const InnerSectionStyle = styled.section`
   display: flex;
   flex-direction: column;
   margin-bottom: 3rem;
+  width: 15rem;
+  @media (max-width: 1100px) {
+    width: 9rem;
+  }
 `;
 
 const InnerSectionTitleStyle = styled.span`
