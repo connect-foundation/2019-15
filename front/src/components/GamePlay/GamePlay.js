7f219cac13cc3bb93c3d04d807a77ad3f1b280cc
Einere <kjwsx23@gmail.com>
2019-12-05 16:37:25 +0900

    fix(game play): add `endQuestionCallback()`, modify `resetQuestionState()`
    문제가 끝난 경우를 대비해서 endQuestion 시그널 핸들러를 추가함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/GamePlay/GamePlay.js
+++ front/src/components/GamePlay/GamePlay.js
@@ -7,10 +7,10 @@ import UserList from 'components/GamePlay/Userlist/Userlist';
 import CanvasSection from 'components/GamePlay/CanvasSection/CanvasSection';
 import Chatting from 'components/GamePlay/Chatting/Chatting';
 import GamePlayContext from 'components/GamePlay/GamePlay.context';
-import GameLoading from 'components/GamePlay/GameLoading/GameLoading';
 import {
   initUserListMsgHandler,
   initGameStartMsgHandler,
+  setStartQuestionHandler,
   setEndQuestionHandler,
   closeSocket,
 } from 'logics/socketLogic';
@@ -34,8 +34,8 @@ const GamePlay = () => {
   // 설정 : Timer 컴포넌트에서 일정 시간이 지났을 때
   // 초기화 : endQuestion 시그널을 받을 때
   const [isOpen, setIsOpen] = useState(false);
-  // 설정 : WordChoice 컴포넌트에서 단어 선택 시
-  // 초기화 : endQuestion 시그널을 받을 때
+  // 설정 : WordChoice 컴포넌트에서 단어 선택 시, endQuestion 시그널을 받을 때
+  // 초기화 : QuestionResult 컴포넌트가 사라질 때
   const [selectedWord, setSelectedWord] = useState('');
   // 설정 : endQuestion 시그널을 받을 때
   // 초기화 : QuestionResult 컴포넌트가 렌더링되고 3초 후
@@ -44,12 +44,25 @@ const GamePlay = () => {
   // 초기화 : QuestionResult 컴포넌트가 렌더링되고 3초 후
   const [scores, setScores] = useState([]);
 
-  function resetQuestionStates() {
-    setPainter(null);
+  function endQuestionCallback({ nextExaminerSocketId, _scores, answer }) {
+    // 결과 화면 띄우기
+    setSelectedWord(answer);
+    setScores(_scores);
+    setShowQuestionResult(true);
+    setTimeout(() => {
+      // 각종 상태 초기화하기
+      resetQuestionStates(nextExaminerSocketId);
+      setShowQuestionResult(false);
+    }, 5000);
+  }
+
+  function resetQuestionStates(nextExaminerSocketId) {
+    setPainter(nextExaminerSocketId);
     setQuestionWord(initialQuestionWordState);
     setIsTimerGetReady(false);
     setIsOpen(false);
     setSelectedWord('');
+    // todo: 캔버스 데이터 초기화
   }
 
   useEffect(() => {
@@ -57,10 +70,10 @@ const GamePlay = () => {
       if (!gameSocket) return;
       initUserListMsgHandler(gameSocket, { setUserList });
       initGameStartMsgHandler(gameSocket, { setPainter });
-        io.setStartQuestionHandler(setQuestionWord, () => {
-            setIsTimerGetReady(true);
-        });
-      setEndQuestionHandler(gameSocket, setScores, setSelectedWord);
+      setStartQuestionHandler(gameSocket, setQuestionWord, () => {
+        setIsTimerGetReady(true);
+      });
+      setEndQuestionHandler(gameSocket, endQuestionCallback);
     };
     initSocket();
 
@@ -73,24 +86,24 @@ const GamePlay = () => {
     return <Redirect to="main" />;
   }
 
-    const contextValue = {
-        userList,
-        setUserList,
-        painter,
-        setPainter,
-        questionWord,
-        setQuestionWord,
-        isTimerGetReady,
-        setIsTimerGetReady,
-        isOpen,
-        setIsOpen,
-        selectedWord,
-        setSelectedWord,
-        showQuestionResult,
-        setShowQuestionResult,
-        scores,
-        setScores,
-    };
+  const contextValue = {
+    userList,
+    setUserList,
+    painter,
+    setPainter,
+    questionWord,
+    setQuestionWord,
+    isTimerGetReady,
+    setIsTimerGetReady,
+    isOpen,
+    setIsOpen,
+    selectedWord,
+    setSelectedWord,
+    showQuestionResult,
+    setShowQuestionResult,
+    scores,
+    setScores,
+  };
 
   return (
     <GamePlayContext.Provider value={contextValue}>
