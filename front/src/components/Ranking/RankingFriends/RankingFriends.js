2598cfe0d2317e4393a4f6f202ce2987990b89c5
changgunyee <minyoung403@naver.com>
2019-12-05 16:58:59 +0900

    feat: make useGetRankingFriends custom hook

--- front/src/components/Ranking/RankingFriends/RankingFriends.js
+++ front/src/components/Ranking/RankingFriends/RankingFriends.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { getRankingFriends } from 'queries/ranking';
 import Loading from 'components/globalComponents/Loading/Loading';
 import Alert from 'components/globalComponents/Alert/Alert';
 import InfinityScroll from 'components/globalComponents/InfinityScroll/InfinityScroll';
+import useGetRankingFriends from 'hooks/Ranking/useGetRankingFriends';
+import RankingAllStyle from 'components/Ranking/RankingAll/RankingAll.style';
 import UserRankingList from '../UserRankingList/UserRankingList';
-import RankingFriendsStyle from './RankingFriends.style';
 
 export default function RankingFriends() {
-  const { data, loading, error, fetchMore } = useQuery(getRankingFriends);
+  const { data, loading, error, fetchMore, hasMore } = useGetRankingFriends();
   if (loading) {
     return <Loading />;
   }
@@ -16,38 +15,14 @@ export default function RankingFriends() {
     return <Alert type="error" />;
   }
 
-  const {
-    pageInfo: { endCursor, hasNextPage },
-    edges,
-  } = data.rankingFriends;
-  if (!edges.length) {
+  if (!data.length) {
     return <Alert type="noData" />;
   }
-
-  const loadMore = () =>
-    fetchMore({
-      query: getRankingFriends,
-      variables: { after: endCursor },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        const newEdges = fetchMoreResult.rankingFriends.edges;
-        const { pageInfo } = fetchMoreResult.rankingFriends;
-        return newEdges.length
-          ? {
-              rankingFriends: {
-                __typename: prev.rankingFriends.__typename,
-                edges: [...prev.rankingFriends.edges, ...newEdges],
-                pageInfo,
-              },
-            }
-          : prev;
-      },
-    });
-  const users = edges.map((edge) => edge.node);
   return (
-    <RankingFriendsStyle>
-      <InfinityScroll loadMore={loadMore} hasMore={hasNextPage}>
-        <UserRankingList users={users} />
+    <RankingAllStyle>
+      <InfinityScroll loadMore={fetchMore} hasMore={hasMore}>
+        <UserRankingList users={data} />
       </InfinityScroll>
-    </RankingFriendsStyle>
+    </RankingAllStyle>
   );
 }
