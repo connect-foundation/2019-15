f170113cbb655fc9088b87bb28bfed08f3e3dd31
changgunyee <minyoung403@naver.com>
2019-12-06 01:14:44 +0900

    feat: add onlineIo disconnect event handler
    접속이 끊어질 때, 접속 중인 친구들에게 offLine임을 알림

--- back/socket/online/disconnect.js
+++ back/socket/online/disconnect.js
@@ -1,5 +1,22 @@
-function disconnect(nodeCache, user) {
-  nodeCache.del(user.id);
+const { Friends } = require('../../db/models');
+
+async function disconnect(nodeCache, user, socket) {
+  user.socketIdList.splice(user.socketIdList.indexOf(socket.id), 1);
+  if (user.socketIdList.length) return;
+
+  const FriendsFound = await Friends.findAll({
+    where: {
+      pFriendId: user.id,
+    },
+  });
+  FriendsFound.forEach(({ dataValues }) => {
+    const friend = nodeCache.get(dataValues.sFriendId);
+    if (friend && friend.socketIdList.length) {
+      friend.socketIdList.forEach((socketId) => {
+        this.onlineIo.to(socketId).emit('friendOffline', user);
+      });
+    }
+  });
 }
 
 module.exports = disconnect;
