e82849689732a2ff0ed8d8b9f56fd97179cf69b1
EasyZero <eminem54@naver.com>
2019-12-06 11:43:25 +0900

    refactor: move Setting out of Preparation
    Setting 컴포넌트와 Preparation 컴포넌트가 분리됨에 따라 위치 이동
    버튼 클릭시 준비페이지로 이동하도록함

---
+++ front/src/components/Setting/Setting.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect } from 'react';
+import GlobalContext from 'global.context';
+import { useHistory } from 'react-router-dom';
+import { emitMakePrivateRoom, emitEnterPrivateRoom } from 'logics/socketLogic';
+import useInput from 'hooks/Input/useInput';
+import Room from 'logics/room';
+import {
+  SettingStyle,
+  RoomSettingStyle,
+  UserSettingStyle,
+  GameStartButtonStyle,
+} from './Setting.style';
+
+export default function Setting() {
+  const { user, room, setRoom, gameSocket } = useContext(GlobalContext);
+  const [nickname, onChange] = useInput('부스트캠퍼');
+  const history = useHistory();
+
+  useEffect(() => {
+    function init() {
+      const secretRoomId = window.location.hash.split('setting:')[1];
+
+      setRoom(new Room(secretRoomId, '비밀방'));
+      if (user.roomOwner) {
+        emitMakePrivateRoom(gameSocket, { roomId: secretRoomId });
+      }
+    }
+    init();
+  }, [gameSocket, setRoom, user.roomOwner]);
+
+  function onClickGameStart() {
+    emitEnterPrivateRoom(gameSocket, { nickname, roomId: room.roomId });
+    history.push(`private:${room.roomId}`);
+  }
+  return (
+    <SettingStyle>
+      <RoomSettingStyle />
+      <UserSettingStyle />
+      <GameStartButtonStyle onClick={onClickGameStart}>
+        게임 시작
+      </GameStartButtonStyle>
+    </SettingStyle>
+  );
+}
