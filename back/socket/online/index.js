d48fb1bf2fa9f13e316ff777ce17015072c1db9a
changgunyee <minyoung403@naver.com>
2019-12-07 14:42:20 +0900

    feat: create deleteFriend socket event
    친구 삭제 시, 서로의 online상태를 없애는 event 구현

--- back/socket/online/index.js
+++ back/socket/online/index.js
@@ -7,6 +7,7 @@ const SocketUser = require('./SocketUser');
 const parseCookies = require('../../util/cookie/parseCookies');
 const { Friends } = require('../../db/models');
 const checkFriendOnline = require('./checkFriendOnline');
+const deleteFriend = require('./deleteFriend');
 
 const nodeCache = new NodeCache({ useClones: false });
 
@@ -16,25 +17,23 @@ async function emitOnline(socket, socketUser) {
       pFriendId: socketUser.id,
     },
   });
+
   const onlineFriends = FriendsFound.reduce((acc, { dataValues }) => {
-    const friendSocket = nodeCache.get(dataValues.sFriendId);
-    if (!friendSocket || !friendSocket.socketList.length) {
-      return acc;
+    const friendSocket = dataValues.sFriendId ? nodeCache.get(dataValues.sFriendId) : null;
+    if (friendSocket) {
+      friendSocket.emitToMySockets('friendsOnline', { [socketUser.id]: socketUser.user });
+      acc[friendSocket.id] = friendSocket.user;
     }
-    friendSocket.socketList.forEach(({ id }) => {
-      this.onlineIo.to(id).emit('friendsOnline', { [socketUser.id]: socketUser.user });
-    });
-    acc[friendSocket.id] = friendSocket.user;
     return acc;
   }, {});
 
   this.onlineIo.to(socket.id).emit('friendsOnline', onlineFriends);
 }
 
-function getOrCreateSocketUser(id, nickname, socket) {
+function getOrCreateSocketUser(id, nickname, socket, io) {
   let socketUser = nodeCache.get(id);
   if (!socketUser) {
-    socketUser = new SocketUser(id, nickname);
+    socketUser = new SocketUser(id, nickname, io);
     nodeCache.set(id, socketUser);
   }
 
@@ -51,7 +50,7 @@ async function setOnlineSockets(socket) {
       subject: jwtOptions.subject,
     });
 
-    socketUser = getOrCreateSocketUser(id, nickname, socket);
+    socketUser = getOrCreateSocketUser(id, nickname, socket, this.onlineIo);
     await emitOnline(socket, socketUser);
   } catch (e) {
     console.log(e);
@@ -60,6 +59,7 @@ async function setOnlineSockets(socket) {
 
   if (!socketUser) return;
 
+  socket.on('deleteFriend', deleteFriend.bind(this, nodeCache, socketUser));
   socket.on('checkFriendOnline', checkFriendOnline.bind(this, nodeCache, socketUser));
   socket.on('requestFriend', requestFriend.bind(this, nodeCache, socketUser));
   socket.on('disconnect', disconnect.bind(this, nodeCache, socketUser, socket));
