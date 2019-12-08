aba08ea5b11bf317d2e9e25b98c07f25681352cd
hos101010 <hos101010@naver.com>
2019-12-09 05:07:33 +0900

    feat: show all chatting only to passed user
    정답을 맞춘 사람들에게만 모든 채팅 공개
    
    문제점) 다음 라운드가 되면
    다시 privileged가 false가 되어서
    그 전 라운드에서 true일 때 했던 채팅이 가려짐
    해결해야함...ㅜ

--- front/src/components/GamePlay/Chatting/ChattingList/ChattingList.js
+++ front/src/components/GamePlay/Chatting/ChattingList/ChattingList.js
@@ -1,15 +1,22 @@
 import React, { useRef, useState, useEffect, useContext } from 'react';
 import GlobalContext from 'global.context';
+import GamePlayContext from 'components/GamePlay/GamePlay.context';
 import { initChattingHandler } from 'logics/socketLogic';
 import ChattingListStyle from './ChattingList.style';
 import Div from './Div.style';
 
 export default function ChattingList() {
   const { gameSocket } = useContext(GlobalContext);
-
+  const { userList } = useContext(GamePlayContext);
   const scrollRef = useRef(null);
   const [messages, pushMessage] = useState([]);
   const [init, setInit] = useState(true);
+  const isPrivileged = userList.findIndex((user) => {
+    if (user.socketId === gameSocket.id) {
+      return user.privileged;
+    }
+    return false;
+  });
 
   useEffect(() => {
     const initSocket = async () => {
@@ -28,6 +35,7 @@ export default function ChattingList() {
     <ChattingListStyle ref={scrollRef}>
       {messages.map((value, idx) => {
         const order = idx + 1;
+        if (isPrivileged === -1 && value.privileged === true) return null;
         return (
           <Div key={order} order={order} privileged={value.privileged}>
             {value.content}
