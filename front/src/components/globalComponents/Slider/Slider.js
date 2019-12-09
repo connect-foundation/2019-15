f52690b29b545589518833d1e726a1892a4d70d4
changgunyee <minyoung403@naver.com>
2019-12-09 20:05:24 +0900

    feat: create Slider global component
    Tools에서 선 굵기 선택을 위해 사용할 Slider 컴포넌트 구현

---
+++ front/src/components/globalComponents/Slider/Slider.js
@@ -0,0 +1,123 @@
+/* eslint no-param-reassign:0 */
+import React, {
+  useReducer,
+  useCallback,
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+  HandlebarStyle,
+  LineStyle,
+  SliderStyle,
+  SliderContainerStyle,
+} from 'components/globalComponents/Slider/Slider.style';
+import { getNodeCenterPos } from 'util/Slider';
+
+Slider.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
+  initialStep: PropTypes.number,
+  unit: PropTypes.number,
+  onChange: PropTypes.func,
+};
+
+Slider.defaultProps = {
+  max: 10,
+  min: 0,
+  initialStep: 5,
+  unit: 1,
+  onChange: () => {},
+};
+
+export default function Slider({ max, min, unit, onChange, initialStep }) {
+  const stepReducer = useCallback(
+    (state, action) => {
+      if (action.type === 'increase')
+        return state + unit <= max ? state + unit : state;
+      if (action.type === 'decrease')
+        return state - unit >= min ? state - unit : state;
+      if (action.type === 'changeStep') {
+        const newStep = state + action.value;
+        return newStep >= min && newStep <= max ? newStep : state;
+      }
+      throw new Error(`${action.type} is wrong action type`);
+    },
+    [max, min, unit],
+  );
+
+  const [handlebarLeft, setHandlebarLeft] = useState(0);
+  const [step, stepDispatch] = useReducer(stepReducer, initialStep);
+  const lineUnitLength = useRef(null);
+
+  useEffect(() => {
+    setHandlebarLeft(((step - min) / unit) * lineUnitLength.current);
+    onChange(step);
+  }, [lineUnitLength, min, onChange, step, unit]);
+
+  const setSliderEvents = useCallback(
+    (sliderNode) => {
+      const handlebarNode = sliderNode.firstChild;
+      lineUnitLength.current =
+        sliderNode.lastChild.clientWidth / ((max - min) / unit);
+      let isDown = false;
+
+      const getLengthDiff = (x) => x - getNodeCenterPos(handlebarNode).x;
+
+      const getStepDiff = (lengthDiff) => {
+        const halfLineUnitLength = lineUnitLength.current / 2;
+        const errorRange =
+          lengthDiff > 0 ? halfLineUnitLength : -halfLineUnitLength;
+        return (
+          parseInt((lengthDiff + errorRange) / lineUnitLength.current, 10) *
+          unit
+        );
+      };
+
+      const onMouseDown = (e) => {
+        e.preventDefault();
+        if (e.target === sliderNode.firstChild) {
+          isDown = true;
+        } else if (e.target === sliderNode.lastChild) {
+          const lengthDiff = getLengthDiff(e.clientX);
+
+          stepDispatch({
+            type: 'changeStep',
+            value: getStepDiff(lengthDiff),
+          });
+        }
+      };
+      const onMouseMove = (e) => {
+        e.preventDefault();
+        if (!isDown) return;
+
+        const diff = getLengthDiff(e.clientX);
+
+        if (diff >= lineUnitLength.current) {
+          stepDispatch({ type: 'increase' });
+        } else if (diff <= -lineUnitLength.current) {
+          stepDispatch({ type: 'decrease' });
+        }
+      };
+      const onMouseEnd = () => {
+        isDown = false;
+      };
+
+      document.addEventListener('mousedown', onMouseDown);
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseEnd);
+      document.addEventListener('mouseleave', onMouseEnd);
+    },
+    [lineUnitLength, max, min, unit],
+  );
+
+  return (
+    <SliderContainerStyle>
+      <SliderStyle ref={setSliderEvents}>
+        <HandlebarStyle left={handlebarLeft} step={step} />
+        <LineStyle />
+      </SliderStyle>
+    </SliderContainerStyle>
+  );
+}
---
+++ front/src/components/globalComponents/Slider/Slider.style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const LineStyle = styled.div`
+  width: 100%;
+  height: 0.3rem;
+  background-color: ${(props) => props.theme.geyser};
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 0.7rem 0 0.7rem 0;
+  background-clip: content-box;
+`;
+
+export const HandlebarStyle = styled.div`
+  width: 1rem;
+  height: 1rem;
+  background-color: #3d3d3b;
+  border-radius: 50%;
+  z-index: 1;
+  position: relative;
+  left: ${(props) => props.left}px;
+`;
+
+export const SliderStyle = styled.div`
+  cursor: pointer;
+  width: 10rem;
+  position: relative;
+`;
+
+export const SliderContainerStyle = styled.div``;
