8401cb7c0c53050c3815494078a91fe074f928f6
EasyZero <eminem54@naver.com>
2019-12-10 19:59:40 +0900

    feat: add gameStart Button
    방장만 누를 수 있는 게임시작 버튼 추가

--- front/src/components/Preparation/GameSetting/GameSetting.js
+++ front/src/components/Preparation/GameSetting/GameSetting.js
@@ -1,6 +1,32 @@
-import React from 'react';
-import { GameSettingStyle } from './GameSetting.style';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { startPrivateGame } from 'logics/socketLogic';
+import GlobalContext from 'global.context';
+import { GameSettingStyle, StartBtn } from './GameSetting.style';
 
-export default function GameSetting() {
-  return <GameSettingStyle>1</GameSettingStyle>;
+GameSetting.propTypes = {
+  roomOwner: PropTypes.bool.isRequired,
+  waitingUserList: PropTypes.arrayOf(
+    PropTypes.shape({
+      nickname: PropTypes.string,
+      socketId: PropTypes.string,
+      avatar: PropTypes.number,
+    }),
+  ).isRequired,
+};
+
+export default function GameSetting({ roomOwner, waitingUserList }) {
+  const { gameSocket, room } = useContext(GlobalContext);
+
+  function clickStartBtn() {
+    if (waitingUserList.length < 2) return;
+    startPrivateGame(gameSocket, { roomId: room.roomId });
+  }
+  return (
+    <GameSettingStyle>
+      <StartBtn disabled={!roomOwner} onClick={clickStartBtn}>
+        게임 시작
+      </StartBtn>
+    </GameSettingStyle>
+  );
 }
