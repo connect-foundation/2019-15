3c7727eb6aed246d527fa840b2eac85041c5dbb1
EasyZero <eminem54@naver.com>
2019-12-10 16:14:40 +0900

    fix: move getCharacter function
    이미지 반환하는 함수를 로직으로 분리

--- front/src/components/Setting/AvatarImg.js
+++ front/src/components/Setting/AvatarImg.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import character1 from 'asset/character1.png';
-import character2 from 'asset/character2.png';
-import character3 from 'asset/character3.png';
+import getCharacter from 'logics/avatar';
 import { AvatarImage } from './Setting.style';
-
-function getCharacter(index) {
-  switch (index) {
-    case 0:
-      return character1;
-
-    case 1:
-      return character2;
-
-    case 2:
-      return character3;
-
-    default:
-      return null;
-  }
-}
 
 AvatarImg.propTypes = {
   avatarIdx: PropTypes.number,
---
+++ front/src/logics/avatar/index.js
@@ -0,0 +1,21 @@
+import character1 from 'asset/character1.png';
+import character2 from 'asset/character2.png';
+import character3 from 'asset/character3.png';
+
+function getCharacter(index) {
+  switch (index) {
+    case 0:
+      return character1;
+
+    case 1:
+      return character2;
+
+    case 2:
+      return character3;
+
+    default:
+      return null;
+  }
+}
+
+export default getCharacter;
