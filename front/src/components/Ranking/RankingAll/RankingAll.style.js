2cc9f712cff9f9999802a91f01b37257f6202161
changgunyee <minyoung403@naver.com>
2019-12-13 11:47:08 +0900

    fix: remove old review files

--- front/src/components/Ranking/RankingAll/RankingAll.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const RankingAllStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export default RankingAllStyle;
--- front/src/components/Ranking/RankingFriends/RankingFriends.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const RankingFriendsStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export default RankingFriendsStyle;
