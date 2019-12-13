8bcb1122891559cf87d41e81b09ccb7af16403e7
EasyZero <eminem54@naver.com>
2019-12-13 14:06:03 +0900

    컴포넌트 또는 페이지 안 useEffect 에 너무 많은 로직이 들어있는데 로직을 분리하려면 컴포넌트 js파일과 같이 두는 방법이 좋은지, 어떤 방법이 선호되는지 궁금합니다.
    useEffect를 분리하는 방법은 커스텀훅이 제일 확실한 방법일까요?
    
    또한 게임플레이 컴포넌트에는 너무 많은 상태가 있어 컴포넌트가 어지러운데 리덕스를 쓰는게 나았을까요?
    
    또한 이벤트 핸들러들도 컴포넌트에서 수행해야 할 로직인데 다른 폴더로 분리하는게 좋을 지 궁금합니다

---
+++ front/components/Waiting/Waiting.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useContext, useState } from "react";
+import { Redirect, useParams, useHistory } from "react-router-dom";
+import Preparation from "components/Preparation/Preparation";
+import {
+  initUserListMsgHandler,
+  initMovePrivateGame,
+  initSetRoomOwner
+} from "logics/socketLogic";
+import GlobalSocket from "global.context";
+import NavigationBar from "components/NavigationBar/NavigationBar";
+
+export default function Waiting() {
+  const { gameSocket, setGameSocket } = useContext(GlobalSocket);
+  const [userList, setUserList] = useState([]);
+  const { hash } = useParams();
+  const history = useHistory();
+  const [roomOwner, setRoomOwner] = useState(false);
+
+  useEffect(() => {
+    if (!gameSocket) return;
+    initUserListMsgHandler(gameSocket, { setUserList });
+    initMovePrivateGame(gameSocket, () => {
+      history.push(`private${hash}`);
+    });
+    initSetRoomOwner(gameSocket, { setRoomOwner });
+  }, [gameSocket, hash, history, setGameSocket]);
+
+  if (!gameSocket || gameSocket.disconnected) {
+    return <Redirect to={`setting${hash}`} />;
+  }
+
+  return (
+    <>
+      <NavigationBar />
+      <Preparation waitingUserList={userList} roomOwner={roomOwner} />
+    </>
+  );
+}
