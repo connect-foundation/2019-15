c8b95077b0adeebf9bbc3d7c6d777b5ab494c497
EasyZero <eminem54@naver.com>
2019-12-15 11:59:24 +0900

    feat: add mypage ChangeCharacter
    마이페이지에 아바타 변경 컴포넌트 구현

--- front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.js
+++ front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Avatar from 'components/Avatar/Avatar';
+import useCarousel from 'hooks/Carousel/useCarousel';
 import ChangeCharacterStyle from './ChangeCharacter.style';
 
 const ChangeCharacter = () => {
-  return <ChangeCharacterStyle id="ChangeCharacter" />;
+  const [avatar, clickLeftBtn, clickRightBtn] = useCarousel(3);
+
+  return (
+    <ChangeCharacterStyle>
+      <Avatar
+        avatar={avatar}
+        clickLeftBtn={clickLeftBtn}
+        clickRightBtn={clickRightBtn}
+      />
+    </ChangeCharacterStyle>
+  );
 };
 
 export default ChangeCharacter;
--- front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.style.js
+++ front/src/components/MyInfoSection/ChangeCharacter/ChangeCharacter.style.js
@@ -4,6 +4,7 @@ const ChangeCharacterStyle = styled.section`
   background-color: dimgray;
   width: 100%;
   height: 100px;
+  padding-top: 10px;
 `;
 
 export default ChangeCharacterStyle;
