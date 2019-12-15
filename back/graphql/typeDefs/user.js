45ea62aa9a19ace2710a2360fee7d46f6de1d4d8
changgunyee <minyoung403@naver.com>
2019-12-15 23:15:02 +0900

    refactor: remove unused query and mutations

--- back/graphql/typeDefs/user.js
+++ back/graphql/typeDefs/user.js
@@ -20,14 +20,10 @@ module.exports = `
   
   extend type Query{
     users:[User]
-    getWordByNickname(nickname:String):Word
     checkNicknameAvailable(nickname:String!):NicknameResult
   }
   
   extend type Mutation{
     changeNickname(nickname:String!):NicknameResult
-    updateUserNicknameById(id:String, nickname:String):[Int]
-    createWord(userId:String, nickname:String):Word
-    updateWordUserIdById(id:String, nickname:String):Word
   }
 `;
