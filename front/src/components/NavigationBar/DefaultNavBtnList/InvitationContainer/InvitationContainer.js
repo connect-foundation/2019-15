27c84bd47d308b23978c446338be9c43bd66d427
changgunyee <minyoung403@naver.com>
2019-12-16 10:57:54 +0900

    refactor: remove alarm from NavBtnList
    알람을 Navbar왼쪽에 따로 구성하게 수정할 예정이므로 알람을 NavBtnList에서 삭제

--- front/src/components/NavigationBar/DefaultNavBtnList/InvitationContainer/InvitationContainer.js
+++ front/src/components/NavigationBar/DefaultNavBtnList/InvitationContainer/InvitationContainer.js
@@ -1,41 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavImageStyle } from 'components/NavigationBar/DefaultNavBtnList/DefaultNavBtnList.style';
 import { faGamepad } from '@fortawesome/free-solid-svg-icons';
 import { FriendRequestContainerStyle } from 'components/NavigationBar/DefaultNavBtnList/FriendRequestContainer/FriendRequestContainer.style';
-import InvitationList from 'components/NavigationBar/DefaultNavBtnList/InvitationContainer/InvitationList/InvitationList';
 import useCloseClicker from 'hooks/useCloseClicker';
-import useNotice from 'hooks/Alarm/useNotice';
-import useAlarm from 'hooks/Alarm/useAlarm';
-import Alarm from 'components/NavigationBar/DefaultNavBtnList/FriendRequestContainer/Alarm';
+import InvitationList from 'components/NavigationBar/DefaultNavBtnList/InvitationContainer/InvitationList/InvitationList';
 
 export default function InvitationContainer() {
-  const [alarmList, alarmListDispatch] = useAlarm();
-  const [notice, noticeDispatch] = useNotice();
-
-  let invitationContent = null;
-  if (notice.isOpen) {
-    invitationContent =
-      notice.type === 'alarm' ? (
-        <Alarm alarmList={alarmList} />
-      ) : (
-        <InvitationList />
-      );
-  }
-
-  const openMessages = () => {
-    noticeDispatch({ open: true, type: 'messages' });
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleIsOpen = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
   };
-
-  const closeMessages = () => {
-    noticeDispatch({ open: false, type: 'messages' });
-  };
-
-  const closeClicker = useCloseClicker(closeMessages);
+  const closeClicker = useCloseClicker(() => {
+    setIsOpen(false);
+  });
 
   return (
     <FriendRequestContainerStyle ref={closeClicker}>
-      <NavImageStyle icon={faGamepad} onClick={openMessages} />
-      {invitationContent}
+      <NavImageStyle icon={faGamepad} onClick={toggleIsOpen} />
+      {isOpen ? <InvitationList /> : null}
     </FriendRequestContainerStyle>
   );
 }
