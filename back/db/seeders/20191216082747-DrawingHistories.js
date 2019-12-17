e7f3f9894ce825abbbc41defb694e64949b579db
hos101010 <hos101010@naver.com>
2019-12-18 07:31:09 +0900

    fix: change seeder

--- back/db/seeders/20191216082747-DrawingHistories.js
+++ back/db/seeders/20191216082747-DrawingHistories.js
@@ -1,9 +1,8 @@
 module.exports = {
   up: (queryInterface) => {
     return queryInterface.bulkInsert('DrawingHistories', [
-      { userId: 4, word: '지져스' },
-      { userId: 4, word: '돼지' },
       { userId: 4, word: '안경' },
+      { userId: 4, word: '돼지' },
     ]);
   },
 
