ab4694aafb1f1cbd5b00007d9f340450f4830fe6
EasyZero <eminem54@naver.com>
2019-12-17 14:45:23 +0900

    refactor: replace select components
    준비페이지에서 방장이 설정한 데이터 함께 시작메시지와 보내도록 설정
    select 들 분리한 컴포넌트로 대체

--- front/src/components/Preparation/GameSetting/GameSetting.js
+++ front/src/components/Preparation/GameSetting/GameSetting.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import GlobalContext from 'global.context';
-import timerConfig from 'constant/timerConfig';
-import Select from 'components/globalComponents/Select/Select';
 import useInput from 'hooks/Input/useInput';
-import { roundOption } from 'constant/room/roomInfo';
 import { GameSettingStyle, StartBtn } from './GameSetting.style';
+import TimeSetting from './TimeSetting';
+import RoundSetting from './RoundSetting';
+import CategorySetting from './CategorySetting';
 
 GameSetting.propTypes = {
   roomOwner: PropTypes.bool.isRequired,
@@ -22,21 +22,26 @@ export default function GameSetting({ roomOwner, waitingUserList }) {
   const { gameSocket, room } = useContext(GlobalContext);
   const [timer, onChangeTimer] = useInput();
   const [round, onChangeRound] = useInput();
+  const [category, onChangeCategory] = useInput();
+
   function clickStartBtn() {
     if (waitingUserList.length < 2) return;
-    gameSocket.emit('startPrivateGame', { roomId: room.roomId, timer, round });
+    gameSocket.emit('startPrivateGame', {
+      roomId: room.roomId,
+      timer,
+      round,
+      category,
+    });
   }
 
   return (
     <GameSettingStyle>
-      {'게임 시간'}
-      <Select
-        option={timerConfig.timerOption}
-        defaultOption={timerConfig.defaultExpireTime / 1000}
-        onChange={onChangeTimer}
+      <TimeSetting disabled={!roomOwner} onChangeTimer={onChangeTimer} />
+      <RoundSetting disabled={!roomOwner} onChangeRound={onChangeRound} />
+      <CategorySetting
+        disabled={!roomOwner}
+        onChangeCategory={onChangeCategory}
       />
-      {'라운드'}
-      <Select option={roundOption} defaultOption={3} onChange={onChangeRound} />
       <StartBtn disabled={!roomOwner} onClick={clickStartBtn}>
         게임 시작
       </StartBtn>
