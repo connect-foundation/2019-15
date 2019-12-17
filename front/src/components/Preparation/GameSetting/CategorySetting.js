11e47baee046c9f202262f7d91c3b170563716a8
EasyZero <eminem54@naver.com>
2019-12-18 06:31:04 +0900

    fix: add defaultOption and make data to object array
    각 select 컴포넌트들에 select 변경사항 적용
    option 객체로
    propTypes 설정

--- front/src/components/Preparation/GameSetting/CategorySetting.js
+++ front/src/components/Preparation/GameSetting/CategorySetting.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'components/globalComponents/Select/Select';
 import GET_CATEGORIES from 'queries/category';
@@ -7,7 +7,7 @@ import { useQuery } from '@apollo/react-hooks';
 CategorySetting.propTypes = {
   onChangeCategory: PropTypes.func.isRequired,
   disabled: PropTypes.bool.isRequired,
-  categoryRef: PropTypes.any.isRequired,
+  categoryRef: PropTypes.shape(useRef).isRequired,
 };
 
 export default function CategorySetting({
@@ -25,19 +25,19 @@ export default function CategorySetting({
       </>
     );
   if (error) return <></>;
+  const options = Object.values(data.getCategories).map((dbData) => {
+    return { text: dbData.category, value: dbData.id };
+  });
 
   return (
     <>
       {'카테고리'}
       <Select
         disabled={disabled}
-        option={
-          loading
-            ? []
-            : Object.values(data.getCategories).map((dbData) => dbData.category)
-        }
+        option={options}
         onChangeSelect={onChangeCategory}
         reference={categoryRef}
+        defaultOption="1"
       />
     </>
   );
