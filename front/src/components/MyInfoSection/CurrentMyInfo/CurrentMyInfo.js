03f96945db1706c1a518339f25ee907190a6a115
Einere <kjwsx23@gmail.com>
2019-12-18 17:14:07 +0900

    feat(user info): show nickname & ranking
    마이 페이지에서, 유저의 닉네임과 랭킹을 보여주도록 수정함.
    
    Signed-off-by: Einere <kjwsx23@gmail.com>

--- front/src/components/MyInfoSection/CurrentMyInfo/CurrentMyInfo.js
+++ front/src/components/MyInfoSection/CurrentMyInfo/CurrentMyInfo.js
@@ -1,19 +1,23 @@
 import React, { useContext } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import GlobalContext from 'global.context';
-import useAvatar from 'hooks/Avatar/useAvatar';
 import CurrentMyInfoStyle from './CurrentMyInfo.style';
+import { GET_RANKING_BY_ID } from '../../../queries/ranking';
 
 export default function CurrentMyInfo() {
   const { user } = useContext(GlobalContext);
+  const { loading, error, data } = useQuery(GET_RANKING_BY_ID, {
+    variables: { id: user.id },
+  });
 
-  const [avatarRef] = useAvatar(user.avatar);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
   return (
     <CurrentMyInfoStyle id="CurrentMyInfo">
-      <div alt="avatar" ref={avatarRef} />
       <div>
-        <span className="user-card nickname">닉네임</span>
-        <span className="user-card ranking">랭킹 xxx위</span>
+        <span className="user-card nickname">{user.nickname}</span>
+        <span className="user-card ranking">랭킹 {data.getRankingById}위</span>
       </div>
     </CurrentMyInfoStyle>
   );
