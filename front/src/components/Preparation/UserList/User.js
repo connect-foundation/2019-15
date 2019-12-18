866f9923a30d347f4b1896ff72b513e09c9ad49c
EasyZero <eminem54@naver.com>
2019-12-18 19:55:48 +0900

    fix: avatar image errer fix

--- front/src/components/Preparation/UserList/User.js
+++ front/src/components/Preparation/UserList/User.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import GlobalContext from 'global.context';
 import crown from 'asset/crown.png';
+import { getAvatar } from 'logics/avatar';
 import { UserStyle, RoomOwner, Text, Avatar } from './UserList.style';
 
 User.propTypes = {
@@ -17,7 +18,7 @@ export default function User({ user }) {
   const { gameSocket } = useContext(GlobalContext);
   return (
     <UserStyle>
-      <Avatar src={user.avatar} />
+      <Avatar src={getAvatar(user.avatar)} />
       <Text roomOwner={user.roomOwner} title={user.nickname}>
         <RoomOwner src={crown} />
         {user.nickname}
