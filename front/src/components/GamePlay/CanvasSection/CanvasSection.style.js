bb99cc046d004d984f7bb53067ea19cea4cdaeab
hos101010 <hos101010@naver.com>
2019-12-20 14:35:33 +0900

    topo

--- front/src/components/GamePlay/CanvasSection/CanvasSection.style.js
+++ front/src/components/GamePlay/CanvasSection/CanvasSection.style.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 const CanvasSectionStyle = styled.div`
   position: relative;
-  margin-top: 0.5rem;
+  margin-top: 4.5rem;
   & > section {
-    margin-top: 4rem;
     box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 10px,
       rgba(0, 0, 0, 0.16) 0px 2px 5px;
     display: flex;
